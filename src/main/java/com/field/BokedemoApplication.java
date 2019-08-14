31bee9b5443f8fb6c107b57af59f03979a4b3001
field <768449566@qq.com>
2019-08-14 09:57:33 +0800

    update

---
+++ src/main/java/com/field/BokedemoApplication.java
@@ -0,0 +1,14 @@
+package com.field;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokedemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BokedemoApplication.class, args);
+    }
+
+}
